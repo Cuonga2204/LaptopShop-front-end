ca907c5fd02518dfc991bbe2730e49555dbc0bff
KLCuong <Cuong.KL215004@sis.hust.edu.vn>
2024-11-18 22:11:18 +0700

    Update1

--- src/App.js
+++ src/App.js
@@ -8,33 +8,24 @@ import '../src/page/CSS/ckeditor.css';
 import '../src/page/CSS/cart.css';
 import '../src/page/CSS/order.css';
 import '../src/page/CSS/orderStatus.css';
-import '../src/page/CSS/admin.css';
+
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './page/Login.jsx';
 import Signup from './page/Signup.jsx';
 import Layout from './layouts/Layout.jsx';
-import AdminLayout from './layouts/AdminLayout.jsx';
+
 import Container from './components/homepage/Container.jsx';
 import ProductPage from './page/ProductPage.jsx';
 import Cart from './page/Cart.jsx';
 import Order from './page/Order.jsx';
 import OrderStatus from './page/OrderStatus.jsx';
-import Admin from './page/Admin.jsx';
-import CreateUser from './components/admin/user/CreateUser.jsx';
-import UpdateUser from './components/admin/user/UpdateUser.jsx';
-import ViewUser from './components/admin/user/ViewUser.jsx';
-import CreateProduct from './components/admin/product/CreateProduct.jsx';
-import UpdateProduct from './components/admin/product/UpdateProduct.jsx';
-import ViewProduct from './components/admin/product/ViewProduct.jsx';
+
 import { CartProvider } from './context/CartContext.js';
-import { AdminProvider } from './context/AdminContext'; // Import AdminProvider
+
 import { ProductProvider } from './context/ProductContext.js';
 import axios from 'axios';
-import ProductTable from './components/admin/product/ProductTable.jsx';
-import UserTable from './components/admin/user/UserTable.jsx';
-import AdminProduct from './page/AdminProduct.jsx';
-import AdminUser from './page/AdminUser.jsx';
+
 function App() {
   const [products, setProducts] = useState([]);
   const [filter, setFilter] = useState("TẤT CẢ");
@@ -55,7 +46,7 @@ function App() {
 
   return (
     <CartProvider>
-      <AdminProvider> {/* Bọc toàn bộ ứng dụng trong AdminProvider */}
+      
         <ProductProvider>
           <Router>
             <div className='App'>
@@ -74,23 +65,12 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
 
-                {/* Routes cho admin */}
-                <Route path="/admin" element={<AdminLayout />}>
-                  <Route path="/admin" element={<Admin />} />
-                  <Route path="user" element={<AdminUser />} />
-                  <Route path="product" element={<AdminProduct />} />
-                  <Route path="user/create" element={<CreateUser />} />
-                  <Route path="user/update/:userId" element={<UpdateUser />} />
-                  <Route path="user/:userId" element={<ViewUser />} />
-                  <Route path="product/create" element={<CreateProduct />} />
-                  <Route path="product/update/:productId" element={<UpdateProduct />} />
-                  <Route path="product/:productId" element={<ViewProduct />} />
-                </Route>
+                
               </Routes>
             </div>
           </Router>
         </ProductProvider>
-      </AdminProvider>
+      
     </CartProvider>
   );
 }
